ee0516245bbb4f82bd420eea30c7dcecddf590fa
Keshav kumar <keshavrathor1998@gmail.com>
2019-11-27 11:01:03 +0530

    increase the job limit

--- webhook.py
+++ webhook.py
@@ -139,7 +139,7 @@ def process_request(req):
             filter_query = {"statusVisible": "enum.Hiring_JobPositionStatusVisible.Public"}
             filter_query.update(parameter)
 
-            show_jobs = job.find(filter_query).limit(3)
+            show_jobs = job.find(filter_query).limit(4)
             print("show jobs",show_jobs)
 
             if show_jobs.count()!=0:
