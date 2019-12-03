3b46e6c3aa0054c1557c65073d1ea3aba05274bf
Keshav kumar <keshavrathor1998@gmail.com>
2019-12-03 15:00:41 +0530

    added download model file

---
+++ download_model.py
@@ -0,0 +1,5 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+gdd.download_file_from_google_drive(file_id='10C56f1DSkWbkBBhokJ9szXM44P9T-KfW',
+                                    dest_path='bidaf\\saved_items\\bidaf_50.h5',
+                                    unzip=False)
