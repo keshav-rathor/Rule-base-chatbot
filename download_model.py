7c2243bcdd7608f3a7ab7018b1e448e96da8e69e
Keshav kumar <keshavrathor1998@gmail.com>
2020-02-13 13:20:20 +0530

    sentiment analysis-1

--- download_model.py
+++ download_model.py
@@ -1,6 +1,6 @@
-from google_drive_downloader import GoogleDriveDownloader as gdd
+#from google_drive_downloader import GoogleDriveDownloader as gdd
 
-gdd.download_file_from_google_drive(file_id='10C56f1DSkWbkBBhokJ9szXM44P9T-KfW',
-                                    dest_path='bidaf\\saved\\bidaf_50.h5',
-                                    unzip=False)
+# gdd.download_file_from_google_drive(file_id='10C56f1DSkWbkBBhokJ9szXM44P9T-KfW',
+#                                     dest_path='bidaf\\saved\\bidaf_50.h5',
+#                                     unzip=False)
 
